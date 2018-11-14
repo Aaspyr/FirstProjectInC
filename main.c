cfb961f0581e78d1e3b62bf164273ec64258b9be
Aaspyr <aaspyr@gmail.com>
2018-11-14 11:53:22 +0100

    Update main.c
    Prints odd numbers from the range <a, b>

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i;
+    int a;
+    int b;
+
+    printf("Podaj pierwsza liczbe: ");
+    scanf("%d",&a);
+    printf("Podaj druga liczbe: ");
+    scanf("%d",&b);
+
+    if (a%2 == 0)
+    {
+        if (b%2 == 0)
+        {
+            for(i=a+1; i<b; i=i+2)
+            {
+                printf("%4d",i);
+            }
+        }
+        else
+        {
+            for(i=a+1; i<=b; i=i+2)
+            {
+                printf("%4d",i);
+            }
+        }
+    }
+    else
+    {
+        if (b%2 == 0)
+        {
+            for(i=a; i<b; i=i+2)
+            {
+                printf("%4d",i);
+            }
+        }
+        else
+        {
+            for(i=a; i<=b; i=i+2)
+            {
+                printf("%4d",i);
+            }
+        }
+    }
+    printf("\n");
+    return 0;
+}
